617207a77fe4b2cf77d58c9e92ddf388211459d7
torresmon235 <torresmon235@gmail.com>
2013-02-12 02:02:27 -0200

    Added license stuff to files

--- crystalgun_common/items/ItemCrystalGunGrass.java
+++ crystalgun_common/items/ItemCrystalGunGrass.java
@@ -1,3 +1,16 @@
+//Copyright (C) 2013  torresmon235
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package torresmon235.crystalgun.items;
 
 import java.util.ArrayList;
@@ -93,7 +106,7 @@ public class ItemCrystalGunGrass extends Item implements ICrystalGun
 	
 	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer)
 	{
-		world.playSoundAtEntity(entityplayer, "crystalgun.shot", 0.7565F, 1.0F + itemRand.nextFloat() / 2);
+		world.playSoundAtEntity(entityplayer, "crystalgun.shot", 0.7565F, 0.0F + itemRand.nextFloat() / 6);
     	if(!world.isRemote)
     	{
     		EntityGrass entity = new EntityGrass(world, entityplayer, effects, effectsextra, red, green, blue);
